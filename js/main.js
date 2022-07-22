bac706f839fc7408d00c470203915edfc6b3c2ad
Scarlet Kim <scarletkim714@gmail.com>
2022-07-22 22:15:57 +0000

    added entry object and code to push entry into data model.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
+/* eslint-disable no-unused-vars */
+/* global data */
+
 var $addEntryButton = document.querySelector('#entry-button');
 var $modalContainer = document.querySelector('.modal-container');
 var $submitForm = document.querySelector('form');
 
 function clickEntry(event) {
-  if (event.target.tagName === 'BUTTON') {
-    $modalContainer.classList.remove('hidden');
-  }
-  // console.log();
+  $modalContainer.classList.remove('hidden');
 }
 
 function clickSubmit(event) {
   event.preventDefault();
   if (event.target.tagName === 'BUTTON') {
     $modalContainer.classList.add('hidden');
+    var entry = {
+      time: '1',
+      entryId: 1,
+      description: ''
+    };
+    var day = $submitForm.elements['day-select'].value;
+    data.entries[day].push(entry);
   }
 }
 
